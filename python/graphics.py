93500e073eddcccce7852a24b98dad4c41b3c5c3
Zachary Stewart <zstewar1@umbc.edu>
2015-04-11 02:50:39 -0400

    updated with tests and for wrappable rectangles

--- python/graphics.py
+++ python/graphics.py
@@ -1,5 +1,6 @@
-import itertools
 import driver
+import itertools
+import math
 
 class Display(object):
     def __init__(self, width=112, height=15):
@@ -17,6 +18,13 @@ class Display(object):
     def draw(self, serial_driver):
         serial_driver.draw(itertools.chain.from_iterable(self.buffer))
 
+    def __str__(self):
+        l = [0] * self.height
+        for r in range(self.height):
+            l[r] = ''.join('#' if i else ' ' for i in self.buffer[r]) + '\n'
+        return ''.join(l)
+                
+
 class Sprite(object):
     def __init__(self, x=0, y=0):
         self.x = x
@@ -26,12 +34,45 @@ class Sprite(object):
         pass
 
 class Rectangle(Sprite):
-    def __init__(self, width, height, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, width, height, *args, wrapx=False, wrapy=False, **kwargs):
+        super().__init__(*args, **kwargs)
         self.width = width
         self.height = height
 
+        self.wrapx = wrapx
+        self.wrapy = wrapy
+
     def draw(self, display):
-        for r in range(max(self.y, 0), min(self.y + self.height + 1, display.height)):
-            for c in range(max(self.x, 0), min(self.x + self.width + 1, display.width)):
-                display.buffer[r][c] = 1
+        for r in range(round(self.y), round(self.y + self.height)):
+            for c in range(round(self.x), round(self.x + self.width)):
+                if (0 < r < display.height or self.wrapy) and (0 < c < display.width or self.wrapx):
+                    display.buffer[r % display.height][c % display.width] = 1
+
+class Circle(Sprite):
+    def __init__(self, radius, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.radius = radius
+
+    def draw(self, display):
+        for r in range(
+                max(int(self.y - self.radius + 0.5), 0),
+                min(int(self.y + self.radius + 0.5) + 1, display.height)):
+            for c in range(
+                    max(int(self.x - self.radius + 0.5), 0),
+                    min(int(self.x + self.radius + 0.5) + 1, display.width)):
+                dx = c - self.x
+                dy = r - self.y
+                if math.sqrt(dx*dx + dy*dy) < self.radius:
+                    display.buffer[r][c] = 1
+
+if __name__ == '__main__':
+    disp = Display()
+    circ = Circle(4, 15/2+1, 15/2)
+    rect = Rectangle(3, 4, 25, 5)
+    wrect = Rectangle(4, 4, 0, -1, wrapx=True, wrapy=True)
+
+    circ.draw(disp)
+    rect.draw(disp)
+    wrect.draw(disp)
+
+    print(disp)
