c8635fbb38c29345df1fb154efe351804e1aef4a
Alexander Bauer <sasha@linux.com>
2015-04-11 07:15:58 -0400

    Use pygame keys for immediate shell updates

--- python/games/shell.py
+++ python/games/shell.py
@@ -1,8 +1,51 @@
 import graphics
 import driver
 import game
+import pygame
 
 class Shell(game.Game):
+    norm_input_keys = [pygame.K_0, pygame.K_1, pygame.K_2, pygame.K_3,
+            pygame.K_4, pygame.K_5, pygame.K_6, pygame.K_7, pygame.K_8,
+            pygame.K_9,
+            pygame.K_a,
+            pygame.K_b,
+            pygame.K_c,
+            pygame.K_d,
+            pygame.K_e,
+            pygame.K_f,
+            pygame.K_g,
+            pygame.K_h,
+            pygame.K_i,
+            pygame.K_j,
+            pygame.K_k,
+            pygame.K_l,
+            pygame.K_m,
+            pygame.K_n,
+            pygame.K_o,
+            pygame.K_p,
+            pygame.K_q,
+            pygame.K_r,
+            pygame.K_s,
+            pygame.K_t,
+            pygame.K_u,
+            pygame.K_v,
+            pygame.K_w,
+            pygame.K_x,
+            pygame.K_y,
+            pygame.K_z
+            # pygame.K_SPACE,
+            # pygame.K_EXCLAIM,
+            # pygame.K_QUOTEDBL,
+            # pygame.K_HASH,
+            # pygame.K_QUOTE,
+            # pygame.K_LEFTPAREN,
+            # pygame.K_HASH,
+            # pygame.K_HASH,
+            # pygame.K_HASH,
+            # pygame.K_HASH,
+            # pygame.K_HASH,
+            ]
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -12,20 +55,18 @@ class Shell(game.Game):
         self.sprites.add(self.prompt)
         self.sprites.add(self.user_in)
 
-    def recv_input(self, evt):
-        if evt == "^H" or evt == "^?":
-            if len(self.user_in.text) > 0:
-                self.user_in_buf = self.user_in_buf[:-1]
-        else:
-            self.user_in_buf += evt
-
-        self.user_in.set_text(self.user_in_buf)
 
     def loop(self):
-        while not self.input_queue.empty():
-            evt = self.input_queue.get()
-            self.recv_input(evt)
+        self.handle_events()
 
+        for key in type(self).norm_input_keys:
+            if self.keys[key] and not self.old_keys[key]:
+                print("Got key: %d" % key)
+                self.user_in_buf += chr(key)
+        if self.keys[pygame.K_BACKSPACE] and not self.old_keys[pygame.K_BACKSPACE] :
+            self.user_in_buf = self.user_in_buf[:-1]
+
+        self.user_in.set_text(self.user_in_buf)
         super().loop()
 
 GAME = Shell
