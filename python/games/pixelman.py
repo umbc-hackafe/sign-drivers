4bfac7f9e9a09678ef75e746a7208693ca4f3af4
Dylan Whichard <dylan@whichard.com>
2015-04-11 02:39:29 -0400

    Add super simple game

---
+++ python/games/pixelman.py
@@ -0,0 +1,24 @@
+from .. import graphics
+from .. import driver
+import game
+
+class PixelMan(game.Game):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.pixel = graphics.Rectangle(1, 1, x=56, y=8))
+        self.sprites.add(pixel)
+
+    def loop(self):
+        while not self.input_queue.empty():
+            evt = self.input_queue.get().upper()
+            if evt == "W":
+                self.pixel.y -= 1
+            elif evt == "A":
+                self.pxxel.x -= 1
+            elif evt == "S":
+                self.pixel.y += 1
+            elif evt == "D":
+                self.pixel.x += 1
+
+        super().loop()
