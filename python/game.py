b63b9dc8055a1eed9aaaa24356767aa054a1f21a
Zachary Stewart <zstewar1@umbc.edu>
2015-04-11 05:50:05 -0400

    added unbuffered input, sort of

--- python/game.py
+++ python/game.py
@@ -1,18 +1,32 @@
 import time
+import pygame
 import queue
+import sys
 
 class Game:
     def __init__(self, graphics, serial, framerate=30):
+        pygame.init()
+        pygame.display.set_mode((50,50))
         self.graphics = graphics
         self.serial = serial
         self.sprites = set()
         self.framerate = framerate
         self.input_queue = queue.Queue()
+        self.old_keys = [False] * 500
+        self.keys = [False] * 500
 
     def send_input(self, name):
         self.input_queue.put(name)
 
     def loop(self):
+        self.old_keys = self.keys[:]
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.KEYDOWN:
+                self.keys[event.key] = True
+            if event.type == pygame.KEYUP:
+                self.keys[event.key] = False
+        
         self.graphics.clear()
         for sprite in self.sprites:
             sprite.draw(self.graphics)
