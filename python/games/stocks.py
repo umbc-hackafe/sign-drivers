29158af51b1604590b965f445b3dc31f14a0bb6e
Zachary Stewart <zstewar1@umbc.edu>
2015-04-11 18:52:43 -0400

    setup stockes to work with update

--- python/games/stocks.py
+++ python/games/stocks.py
@@ -11,6 +11,7 @@ URL = "http://finance.yahoo.com/webservice/v1/symbols/{}/quote?format=json"
 class Stocks(game.Game):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
+    self.st = iter([])
     
   def scroll(self, title, text):
     titlesprite = graphics.TextSprite(title, x=112, y=0, width=5, height=7)
@@ -18,20 +19,14 @@ class Stocks(game.Game):
     sprite = graphics.TextSprite(text, x=112, y=8, width=5, height=7)
     self.sprites.add(sprite)
     while (-1)*((sprite.width+1)*len(text)) < sprite.x:
-      self.graphics.clear()
-      for x in self.sprites:
-        x.draw(self.graphics)
-      time.sleep(0.02)
+      yield
       sprite.x -= 1
       titlesprite.x -= 1
-      self.graphics.draw(self.serial)
-    self.graphics.clear()
     self.sprites.remove(sprite)
     self.sprites.remove(titlesprite)
 
-  def loop(self):
+  def stockticker(self, results):
     stocks = []
-    results = json.loads(requests.get(URL.format(','.join(SYMBOLS))).text)
     for res in results["list"]["resources"]:
       stocks.append((res["resource"]["fields"]["symbol"], round(float(res["resource"]["fields"]["price"]), 2)))
 
@@ -42,7 +37,16 @@ class Stocks(game.Game):
       b = '    '.join([' '.join((s, str(v))) for s, v in stocks_bottom])
       if len(b) < len(a):
         a, b = b, a
-      self.scroll(a, b)
-    time.sleep(2)
+        yield from self.scroll(a, b)    
+    
+  def loop(self):
+    try:
+      next(self.st)
+    except StopIteration:
+      results = json.loads(requests.get(URL.format(','.join(SYMBOLS))).text)
+      self.st = self.stockticker(results)
+      time.sleep(2)
+
+    super().loop()
 
 GAME = Stocks
