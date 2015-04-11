28ccaf49cb11efb09c328baae93b070d092eb620
Alexander Bauer <sasha@linux.com>
2015-04-11 15:45:59 -0400

    Pause Space Invaders until a move is made

--- python/games/space.py
+++ python/games/space.py
@@ -17,6 +17,7 @@ class Space(game.Game):
         self.start()
 
     def start(self):
+        self.paused = True
         if self.end == True:
             time.sleep(3)
         self.end = False
@@ -46,70 +47,75 @@ class Space(game.Game):
     def loop(self):
         fire = False
         if 's' in self.keys or 'a' in self.keys:
+            self.paused = False
             self.ship.y += 1
             if self.ship.y > 15:
                 self.ship.y = 15
         if 'w' in self.keys or 'd' in self.keys:
+            self.paused = False
             self.ship.y -= 1
             if self.ship.y < 0:
                 self.ship.y = 0
         if ' ' in self.keys:
+            self.paused = False
             fire = True
-        lowerY = -1
-        upperY = 17
-        for i in self.invaders:
-           if i.y > lowerY:
-               lowerY = i.y
-           if i.y < upperY:
-               upperY = i.y
-        if lowerY == 15:
-            self.direction *= -1
+
+        if not self.paused:
+            lowerY = -1
+            upperY = 17
             for i in self.invaders:
+               if i.y > lowerY:
+                   lowerY = i.y
+               if i.y < upperY:
+                   upperY = i.y
+            if lowerY == 15:
+                self.direction *= -1
+                for i in self.invaders:
+                    i.x += 1
+            if upperY == 0:
+                self.direction *= -1
+            for i in self.invaders:
+                i.y += self.direction
+            if fire:
+                newBullet = graphics.Rectangle(x=self.ship.x, y=self.ship.y, width=1, height=1)
+                self.bullets.append(newBullet)
+            for i in self.invaders:
+                if random.random() < 0.0051:
+                    newBullet = graphics.Rectangle(x=i.x, y=i.y, width=1, height=1)
+                    self.enemyBullets.append(newBullet)
+                    self.sprites.add(newBullet)
+            for i in self.enemyBullets:
                 i.x += 1
-        if upperY == 0:
-            self.direction *= -1
-        for i in self.invaders:
-            i.y += self.direction
-        if fire:
-            newBullet = graphics.Rectangle(x=self.ship.x, y=self.ship.y, width=1, height=1)
-            self.bullets.append(newBullet)
-        for i in self.invaders:
-            if random.random() < 0.0051:
-                newBullet = graphics.Rectangle(x=i.x, y=i.y, width=1, height=1)
-                self.enemyBullets.append(newBullet)
-                self.sprites.add(newBullet)
-        for i in self.enemyBullets:
-            i.x += 1
-            if i.x == self.ship.x and i.y == self.ship.y:
-                self.lose()
-        toremove = list()
-        invadersToRemove = list()
-        for i in self.bullets:
-            if i.x < 0:
-                toremove.append(i)
-            i.x -= 1
-            if not self.end:
-                self.sprites.add(i)
-            if i.x < 0:
-                if i in self.sprites:
-                    self.sprites.discard(i)
-            for j in self.invaders:
-                if j.x <= i.x < (j.x+3) and j.y == i.y:
-                    invadersToRemove.append(j)
-                    self.sprites.discard(j)
-                    self.sprites.discard(i)
+                if i.x == self.ship.x and i.y == self.ship.y:
+                    self.lose()
+            toremove = list()
+            invadersToRemove = list()
+            for i in self.bullets:
+                if i.x < 0:
                     toremove.append(i)
-        for i in invadersToRemove:
-            if i in self.invaders:
-                self.invaders.remove(i)
-        for i in toremove:
-            if i in self.bullets:
-                self.bullets.remove(i)
-        if not(self.invaders):
-            self.win()
-        for i in self.invaders:
-            if i.x > 108:
-                self.lose()
+                i.x -= 1
+                if not self.end:
+                    self.sprites.add(i)
+                if i.x < 0:
+                    if i in self.sprites:
+                        self.sprites.discard(i)
+                for j in self.invaders:
+                    if j.x <= i.x < (j.x+3) and j.y == i.y:
+                        invadersToRemove.append(j)
+                        self.sprites.discard(j)
+                        self.sprites.discard(i)
+                        toremove.append(i)
+            for i in invadersToRemove:
+                if i in self.invaders:
+                    self.invaders.remove(i)
+            for i in toremove:
+                if i in self.bullets:
+                    self.bullets.remove(i)
+            if not(self.invaders):
+                self.win()
+            for i in self.invaders:
+                if i.x > 108:
+                    self.lose()
         super().loop()
         if self.end:
             self.start()
