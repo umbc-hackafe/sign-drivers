a232c78044521f2ec11d4f197e0cb9144c36ba7b
Dylan Whichard <dylan@whichard.com>
2015-04-12 01:27:54 -0400

    More debug

--- python/game.py
+++ python/game.py
@@ -79,7 +79,10 @@ class Game:
         print("Triggering {}.{}".format(item, act))
         if item == "alert" and requests:
             requests.get("localhost:5000/{}/a/7".format(act))
+            print("alert " + act)
         elif item == "buzzer" and gpio:
             gpio.output(BUZZER, 1 if act == "on" else 0)
+            print("buzzer " + act)
         elif item == "beeper" and gpio:
             gpio.output(BEEPER, 1 if act == "on" else 0)
+            print("beeper " + act)
