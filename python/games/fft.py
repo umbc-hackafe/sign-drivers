11af29a5e1dddd5073d6b65263de994b6527bb1f
Mark Murnane <mark25@umbc.edu>
2015-08-21 20:09:53 -0400

    Updated fft code to work on pi

--- python/games/fft.py
+++ python/games/fft.py
@@ -14,24 +14,24 @@ cols = 112
 
 class FFT(game.Game):
     def __init__(self, *args, **kwargs):
-        self.plotMax = 3
-        self.offset = 11
+        self.plotMax = 2
+        self.offset = 9
         self.plotStart = 5
         self.plotEnd = 100
         self.periodsize = 2400
-        self.bufferLength = 9600
+        self.bufferLength = 4800
         self.sampleRate = 48000
         self.buffer = collections.deque(self.bufferLength*[0], self.bufferLength)
 
-        card = 'default'
-        self.inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK, card)
+        card = 'default:CARD=Set'
+        self.inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL, card)
         self.inp.setchannels(1)
         self.inp.setrate(self.sampleRate)
         self.inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
-        self.inp.setperiodsize(self.periodsize)
+        self.periodsize = self.inp.setperiodsize(self.periodsize)
 
         for i in range(0, int(self.bufferLength/self.periodsize)):
-            self.buffer.extend(np.fromstring(self.inp.read()[1], dtype="<h"))
+            self.buffer.extend(np.fromstring(self.inp.read()[1], dtype=">h"))
 
         with open("./fft_data", "r") as datafile:
             self.noiseOffset = json.loads(datafile.read())
@@ -39,14 +39,16 @@ class FFT(game.Game):
         super().__init__(*args, **kwargs)
 
     def loop(self):
-        length, data = self.inp.read()
+        datalength, data = self.inp.read()
+        if (datalength != self.periodsize):
+            return
         self.buffer.extend(np.fromstring(data, dtype="<h"))
         fftData = np.absolute(np.fft.rfft(self.buffer))
         buckets = []
         for i in range(len(fftData)):
             buckets.append(fftData[i]*math.log(i+1))
-        buckets = np.array_split(fftData[:-3000], cols)
-        buckets = [math.log(sum(x)/len(x)+1)-self.offset for x in buckets]
+        buckets = np.array_split(fftData, cols)
+        buckets = [math.log(sum(x)/(len(x)+1)+1)-self.offset for x in buckets]
 #        with open("./fft_data", "w") as datafile:
 #            datafile.write(json.dumps(buckets))
         for i in range(len(buckets)):
